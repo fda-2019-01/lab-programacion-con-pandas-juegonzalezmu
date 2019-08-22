740aede2ae5c1f32fb5ae184ae78877396f81f27
Juan Esteban Gonzalez Murcia <50414971+juegonzalezmu@users.noreply.github.com>
2019-08-22 09:22:50 -0500

    Update q11.py

--- q11.py
+++ q11.py
@@ -3,4 +3,12 @@
 ## separada por ',' de los valores de la columna _c5a
 ## y _c5b (unidos por ':') de la tabla tbl2.tsv
 ## 
+import pandas as pd
+tbl2 = pd.read_csv('tbl2.tsv', sep='\t')
+tbl2['_c5a'] = tbl2['_c5a'].astype(str)
+tbl2['_c5b'] = tbl2['_c5b'].astype(str)
 
+data = pd.DataFrame({
+    'lista': tbl2.groupby(['_c0']).apply(lambda x: ','.join(sorted(x['_c5a'] + ':' + x['_c5b'])))
+}).reset_index()
+print(data)
