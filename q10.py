08715e3411da6c614f217709bf6038b36ae76273
Juan Esteban Gonzalez Murcia <50414971+juegonzalezmu@users.noreply.github.com>
2019-08-22 09:15:20 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -4,3 +4,11 @@
 ## de la tabla tbl1.tsv
 ## 
 
+import pandas as pd
+import numpy as np
+tbl1 = pd.read_csv("tbl1.tsv",
+                          sep = '\t',
+                          thousands = None,
+                          decimal = '.')
+data = pd.DataFrame({'x' : tbl1.groupby( ["_c0"] ).apply(lambda x: ':'.join(sorted(x['_c4']))) }).reset_index()
+print(data)
