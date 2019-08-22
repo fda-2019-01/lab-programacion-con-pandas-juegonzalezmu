0c7d8bb49c3e500769ce402c96ab992ba549c588
Juan Esteban Gonzalez Murcia <50414971+juegonzalezmu@users.noreply.github.com>
2019-08-22 09:06:18 -0500

    Update q09.py

--- q09.py
+++ q09.py
@@ -3,5 +3,14 @@
 ## separada por ':' de los valores de la columna _c2
 ## para el archivo tbl0.tsv
 ## 
-
+import pandas as pd
+import numpy as np
+tbl0 = pd.read_csv("tbl0.tsv",
+                          sep = '\t',
+                          thousands = None,
+                          decimal = '.')
+tbl0['_c2'] = tbl0['_c2'].astype(str)
+data = pd.DataFrame({'x' : tbl0.groupby( ["_c1"] ).apply(lambda x: ':'.join(sorted(x['_c2']))) }).reset_index()
+data.columns = ['_c0', 'x']
+print(data)
 
